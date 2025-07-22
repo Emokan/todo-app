c1bb8c63a53406cde81719ffc499053f61cbeb79
Emokan <emirhan.akgnemo@gmail.com>
2025-07-22 15:16:37 +0300

    feat: add delete todo route

--- todo-backend/routes/todoRoutes.js
+++ todo-backend/routes/todoRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("C:\\Users\\emirh\\todo-app\\todo-backend\\middleware\\authMiddleware");
-const { getTodos, createTodo } = require("C:\\Users\\emirh\\todo-app\\todo-backend\\controllers\\todoController");
+const { getTodos, createTodo, deleteTodo } = require("C:\\Users\\emirh\\todo-app\\todo-backend\\controllers\\todoController");
 
 router.get("/", authMiddleware, getTodos);
 router.post("/", authMiddleware, createTodo);
+router.delete("/:id", authMiddleware, deleteTodo);
 
 module.exports = router;
