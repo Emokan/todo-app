5f6f408314e96cf63bd5db8c04344466e396193a
Emokan <emirhan.akgnemo@gmail.com>
2025-07-23 12:26:53 +0300

    feat: add update functionality for todos route

--- todo-backend/routes/todoRoutes.js
+++ todo-backend/routes/todoRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("C:\\Users\\emirh\\todo-app\\todo-backend\\middleware\\authMiddleware");
-const { getTodos, createTodo, deleteTodo } = require("C:\\Users\\emirh\\todo-app\\todo-backend\\controllers\\todoController");
+const { getTodos, createTodo, deleteTodo, updateTodo } = require("C:\\Users\\emirh\\todo-app\\todo-backend\\controllers\\todoController");
 
 router.get("/", authMiddleware, getTodos);
 router.post("/", authMiddleware, createTodo);
 router.delete("/:id", authMiddleware, deleteTodo);
+router.put("/:id", authMiddleware,updateTodo);
 
 module.exports = router;
