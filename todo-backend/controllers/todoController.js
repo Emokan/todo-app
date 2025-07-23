3c5b94df0a9357a3822d85cba56a495845e878d7
Emokan <emirhan.akgnemo@gmail.com>
2025-07-23 12:23:02 +0300

    feat: implement update functionality for todos

--- todo-backend/controllers/todoController.js
+++ todo-backend/controllers/todoController.js
@@ -39,3 +39,27 @@ exports.deleteTodo = async (req, res) => {
         res.status(500).json({ message: "Silme hatasi.", error: err.mesage });
     }
 };
+
+exports.updateTodo = async (req, res) => {
+    const { id } = req.params;
+    const { text } = req.body;
+
+    try {
+        const todo = await Todo.findById(id);
+
+        if (!todo) {
+            return res.status(404).json({ message: "Görev bulunamadi." });
+        }
+
+        if (todo.userId.toString() !== req.user.id) {
+            return res.status(403).json({ message: "Yetkisiz." });
+        }
+
+        todo.text = text;
+        await todo.save();
+
+        res.json({ message: "Görev güncellendi.", todo });
+    } catch (err) {
+        res.status(500).json({ message: "Güncelleme hatasi.", error: err.message });
+    }
+};
