77bc0a0ce6be176f22a54a1f8c19c71c626d685f
Emokan <emirhan.akgnemo@gmail.com>
2025-07-24 17:22:32 +0300

    feat: add Tailwind CSS setup with PostCSS and Autoprefixer

--- todo-frontend/src/index.js
+++ todo-frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from 'C:\\Users\\emirh\\todo-app\\todo-frontend\\src\\App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
